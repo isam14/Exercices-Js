5c36c7d3f6db6e49c33d55995f1de38fe5085985
isam14 <imuck14@gmail.com>
2017-07-25 16:39:59 +0200

    ajout initial

---
+++ calculatrice/app.js
@@ -0,0 +1,15 @@
+
+
+$("#buttonForm").click(function () {
+var input1 = $("#input1").val();
+var input2 = $("#input2").val ();
+var operator = $("#selectOperation").val();
+
+
+
+
+
+});
+
+$("#result").text    (result);
+
---
+++ calculatrice/app2.js
@@ -0,0 +1,9 @@
+    $("button").click(function () {
+        var input = ($(this).attr("id").charAt(4));
+        var array = [];
+        array.push();
+        console.log(($(this).attr("id").charAt(4)));
+        
+
+})
+
